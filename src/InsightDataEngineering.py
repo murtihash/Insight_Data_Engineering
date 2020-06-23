fd302132351926f9e8e8bf61537084e485fcc043
Mohammad Murtaza Aun Hashmi <murtaza.hashmi94@gmail.com>
2020-06-23 10:58:07 -0400

    Create InsightDataEngineering.py

---
+++ src/InsightDataEngineering.py
@@ -0,0 +1,103 @@
+import csv 
+import sys
+import itertools as ITER
+import collections as COLL
+import operator as OPR
+
+
+"""
+
+Csv Reader from csv module to populate only required columns,
+and change required name columns to lower case, select year from date.
+Used next(reader) to skip first line, one other way to read could be with dictreader and
+use name of columns to get them, but using plain csvreader is slightly faster. 
+
+IF input data does not always have the same order of columns, then DictReader should be used..
+
+"""
+
+
+
+def get_data(input_location):
+    
+    with open(input_location, newline='') as f:
+        
+        datastack=[]
+        
+        reader = csv.reader(f, delimiter=',')
+        next(reader)                 #SKIP first line of headers
+        for row in reader:
+            datastack.append([row[1].lower(),row[0][:4],row[7].lower()])
+            
+            
+    return datastack
+
+
+    
+
+"""
+
+-itertools.groupby to group on the two keys for unique pairs (after sorting on keys).
+-len(a) is total number of complaint for group.
+-Counter from collections to run counts(total distinct companies,% of most_common).
+-itemgetter from operator(c level) for efficient index pull.
+-Yield to create a generator from the groupby iterator for each row. 
+
+"""
+
+
+def prepare_data(data):
+ 
+    key_func = OPR.itemgetter(0,1) 
+    
+    data.sort(key = key_func)
+    
+    for k,v in ITER.groupby(data, key_func): 
+        
+        a=tuple(OPR.itemgetter(2)(x) for x in [*v])
+        
+        yield (k[0],k[1],len(a),len(COLL.Counter(a).keys()),round((COLL.Counter(a).most_common(1)[0][1]/len(a))*100))
+
+
+        
+        
+        
+        
+        
+"""
+
+csv writer to write rows to output efficiently by sending prepare_data generator
+to csv writerows
+
+"""
+
+
+def write_output(output_location,data):
+    
+    with open(output_location, 'w') as f:
+        
+        writer = csv.writer(f, delimiter=',')
+       
+        
+        writer.writerows(prepare_data(data))
+            
+"""
+
+main function to apply functions accordingly
+
+"""
+
+def main():
+    
+    location = sys.argv[1]
+    
+    dataframe = get_data(location)
+    
+    output_location= sys.argv[2]
+    
+    write_output(output_location,dataframe)
+    
+    
+if __name__ == '__main__':
+    main()
+    
